bcb62e42ffa6a2dbe624794ddc2da5699d325137
music-fisher <nkh252550@gmail.com>
2020-12-23 09:20:42 +0000

    [add]controller/users/posts/homes

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,13 @@
+class PostsController < ApplicationController
+  def show
+  end
+
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'posts/show'
+  get 'posts/index'
+  get 'posts/new'
+  get 'posts/edit'
+  root 'homes#top'
+  get 'homes/about'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show]
+  resources :posts
 end
+
+
