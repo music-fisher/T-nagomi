c6bd129e3a7d74117a8284a7ea90d95af301aa0f
music-fisher <nkh252550@gmail.com>
2020-12-24 15:10:57 +0000

    [add]post/new

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,8 +6,23 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
-
+  def create
+    @post = Post.new
+    if @post.user_id == current_user.id
+      @post.save(post_params)
+      redirect_to post_path(@post)
+    end
+  end
+  
   def edit
+  end
+  def update
+  end
+  
+  private
+  def post_params
+    params.require(:post).permit(:title, :body, :kind, :post_image)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
+  belongs_to :user
   attachment :post_image
+  enum kind: {抹茶: 0,玉露: 1, 煎茶: 2,番茶: 3, その他: 4}
 end
