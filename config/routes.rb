3e1c233bc2f573a0f471e4739e035e5a29e594fe
music-fisher <nkh252550@gmail.com>
2020-12-28 14:37:41 +0000

    [edit]mypage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'homes#top'
   get 'homes/about'
+  get '/mypage' => 'users#mypage'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:show, :edit, :update,:index]
