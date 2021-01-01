2c00309c06f2fb31193d06c7542b728ee1990e1c
music-fisher <nkh252550@gmail.com>
2021-01-01 06:12:14 +0000

    [edit]lyout

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,5 +1,5 @@
 class LikesController < ApplicationController
-  
+  before_action :authenticate_user!
   def create
     post = Post.find(params[:post_id])
     like = current_user.likes.new(post_id: post.id)
