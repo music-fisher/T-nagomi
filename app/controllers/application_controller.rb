89881d5679855db0f30fed27cc6d1bfc890a7085
music-fisher <nkh252550@gmail.com>
2021-01-05 13:04:54 +0000

    [fix]kind検索error修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_search
-  
+
     def after_sign_in_path_for(resource)
         user_path(resource)
     end
@@ -10,9 +10,9 @@ class ApplicationController < ActionController::Base
     end
     def set_search
       @search = Post.ransack(params[:q])
-      @search_posts = @search.result.page(params[:page])
+      @search_posts = @search.result.page(params[:page]) if params[:q].present?
     end
-  
+
   protected
 
   def configure_permitted_parameters
