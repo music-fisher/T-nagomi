9419e26da104c17c1dfd3ea00df8f49cae3ab05d
music-fisher <nkh252550@gmail.com>
2021-01-10 14:31:56 +0000

    [add]ゲストログイン

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,11 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :check_guest, only: :destroy
+
+  def check_guest
+    if resource.email == 'guest@example.com'
+      redirect_to root_path
+      flash[:alert] = 'ゲストユーザーは削除できません。'
+    end
+  end
+
+end
