9a1baaa43e310736c6fdb4a4f7b88612ff707762
music-fisher <nkh252550@gmail.com>
2021-01-29 14:23:43 +0000

    [add]notificationコントローラー

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,2 +1,14 @@
 class NotificationsController < ApplicationController
+  def index
+    # current_userの投稿に紐づいた通知
+    @notifications = current_user.passive_notifications
+    # indexに一度も遷移していない通知
+    @notifications.where(checked: false).each do |notification|
+      notification.update_attributes(chacked: true)
+    end
+  end
+  
+  def destroy_all
+    @notifications = current_user.passive_notifications.destroy_all
+  end
 end
--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,8 +1,11 @@
 class RelationshipsController < ApplicationController
   before_action :authenticate_user!
   def create
-    current_user.follow(params[:user_id])
-    
+    # 修正？
+    @user = User.find(params[:user_id])
+    # current_user.follow(params[:user_id])
+    current_user.follow(@user)
+    @user.create_notification_follow!(current_user)
     redirect_to request.referer
   end
   def destroy
