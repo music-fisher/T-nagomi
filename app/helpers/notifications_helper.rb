a269b446529b24812afeac2e73241a7a0101c837
music-fisher <nkh252550@gmail.com>
2021-01-30 13:05:02 +0000

    [add]notification/view作成

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -1,2 +1,21 @@
 module NotificationsHelper
+  def notification_form(notification)
+    @visiter = notification.visiter
+    @comment = nil
+    @visiter_comment = notification.comment_id
+    # notification.actionがどの値か
+    case notification.action
+      when "follow" then
+        tag.a(notification.visiter.name, href: user_path(@visiter))+"があなたをフォローしました"
+      when "like" then
+        tag.a(notification.visiter.name, href: user_path(@visiter))+"が"+tag.a('あなたの投稿', href: post_path(notification.post_id))+"にいいねしました"
+      when "comment" then
+        @comment = Comment.find_by(id: @visiter_comment)&.content
+        tag.a(@visiter.name, href: user_path(@visiter))+"が"+tag.a('あなたの投稿', href: post_path(notification.post_id))+"にコメントしました"
+    end
+  end
+  # 未確認の通知はマークで知らせる
+  # def unchecked_notifications
+  #   @notifications = current_user.passive_notifications.where(checked: false)
+  # end
 end
