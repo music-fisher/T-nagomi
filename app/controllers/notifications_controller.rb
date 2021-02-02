913609a33f492043698c65c6813ca094c49251d5
music-fisher <nkh252550@gmail.com>
2021-02-02 14:30:19 +0000

    [add]通知機能ページネーション

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,7 +1,7 @@
 class NotificationsController < ApplicationController
   def index
     # current_userの投稿に紐づいた通知
-    @notifications = current_user.passive_notifications
+    @notifications = current_user.passive_notifications.page(params[:page]).per(10)
     # indexに一度も遷移していない通知
     @notifications.where(checked: false).each do |notification|
       notification.update_attributes(checked: true)
